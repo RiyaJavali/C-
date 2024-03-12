3b2f23fa198ea611f789443fdcca813aaac7ca2d
RiyaJavali <104997739+RiyaJavali@users.noreply.github.com>
2024-03-12 09:51:56 +0530

    Add files via upload

---
+++ cel_to_far/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float celcius,farenheit;
+    cout<<"Enter the celcius temp"<<endl;
+    cin>>celcius;
+    farenheit=(celcius*9/5)+32;
+    cout<<" Farenheit temp : "<<farenheit;
+    return 0;
+}
---
+++ rev_integer/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,rev=0,rem;
+    cout<<"Enter the integer"<<endl;
+    cin>>n;
+    while(n!=0)
+    {
+        rem=n%10;
+        rev=rev*10+rem;
+        n/=10;
+    }
+    cout<<"Reversed integer = "<<rev;
+    return 0;
+}
